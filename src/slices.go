ddf158d97be819afa28f619e99eff43fe0758ace
hangfire1106 <coops1106@gmail.com>
2017-06-12 14:15:25 +0100

    Slices ex

---
+++ src/slices.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dy)
+
+	for x := range a {
+		a[x] = make([]uint8, dx)
+		for y := range a[x] {
+			a[x][y] = uint8((x^y))
+		}
+	}
+	return a
+}
+
+func main() {
+	pic.Show(Pic) //"http://data:image/png;base64,"
+}
